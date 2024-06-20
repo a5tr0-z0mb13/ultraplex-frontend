56d56b6645bfb6d35aa8dde219cd8cac8ab4dbf0
Chris Jones <jones.c.1978@gmail.com>
2024-06-20 15:50:23 +0100

    install prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,8 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+const prettier = require("eslint-config-prettier");
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
@@ -11,6 +12,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      prettier,
     ],
     processor: angular.processInlineTemplates,
     rules: {
@@ -37,6 +39,7 @@ module.exports = tseslint.config(
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
+      prettier,
     ],
     rules: {},
   }
