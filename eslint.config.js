a4daccbef506fe4b3926837ed35e29da476ab530
Chris Jones <jones.c.1978@gmail.com>
2024-06-20 17:37:49 +0100

    add material

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ module.exports = tseslint.config(
         "error",
         {
           type: "attribute",
-          prefix: "app",
+          prefix: "ultraplex",
           style: "camelCase",
         },
       ],
@@ -28,7 +28,7 @@ module.exports = tseslint.config(
         "error",
         {
           type: "element",
-          prefix: "app",
+          prefix: "ultraplex",
           style: "kebab-case",
         },
       ],
